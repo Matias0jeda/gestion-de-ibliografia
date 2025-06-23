5af20e7f4d1237f8ba6757356281d7dc2037ee78
Matias0jeda <bergottinidavid@mgmail.com>
2025-06-22 21:27:49 -0300

    claude6

--- db.js
+++ db.js
@@ -1,24 +1,9 @@
 const { Pool } = require('pg');
-const {
-    DB_HOST,
-    DB_NAME,
-    DB_PASSWORD,
-    DB_USER,
-    DB_PORT,
-} = require('./config.js');
 
-console.log('💾 Intentando conectar a la DB con:');
-console.log('Host:', DB_HOST);
-console.log('User:', DB_USER);
-console.log('Database:', DB_NAME);
-console.log('Port:', DB_PORT);
+console.log('🔗 DATABASE_URL disponible:', process.env.DATABASE_URL ? 'SÍ' : 'NO');
 
 const pool = new Pool({
-  user: DB_USER,
-  host: DB_HOST,
-  database: DB_NAME,
-  password: DB_PASSWORD,
-  port: DB_PORT,
+  connectionString: process.env.DATABASE_URL,
 });
 
 module.exports = pool;
